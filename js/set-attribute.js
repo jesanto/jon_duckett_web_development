ca83f5c23029b9537f578d33820ba3062f0e3c46
Jason Espiritu Santo <jesp@shaw.ca>
2021-06-23 13:18:41 -0700

    Add files via upload

---
+++ js/set-attribute.js
@@ -0,0 +1,6 @@
+var firstItem = document.getElementById('one');                 // Get the first item 
+firstItem.className = 'complete';                               // Change its class attribute
+
+var fourthItem = document.getElementsByTagName('li').item(3);   // get fourth item 
+// NOTE: The following line should say fourthItem(not el2)
+fourthItem.setAttribute('class', 'coll');                       // Add an attribute to it 
